0c55b14d4b68d26ded4f37fafcdce7c39cd4a126
Rodion Konioshko <rudykonio@gmail.com>
2020-04-02 08:22:42 +0300

    Initial Commit

---
+++ covid-19 tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-19 tracker
+//
+//  Created by Rodion Konioshko on 02/04/2020.
+//  Copyright © 2020 Rodion Konioshko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
